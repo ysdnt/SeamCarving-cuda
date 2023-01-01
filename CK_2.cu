f641659def546284adcd3e67655fff93c6cb70ec
Khanh Toan <laikhanhtoanb10@gmail.com>
2023-01-01 22:52:18 +0700

    nham file nen update lai

--- CK_2.cu
+++ CK_2.cu
@@ -15,42 +15,43 @@
 
 struct GpuTimer
 {
-    cudaEvent_t start;
-    cudaEvent_t stop;
+	cudaEvent_t start;
+	cudaEvent_t stop;
 
-    GpuTimer()
-    {
-        cudaEventCreate(&start);
-        cudaEventCreate(&stop);
-    }
+	GpuTimer()
+	{
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
+	}
 
-    ~GpuTimer()
-    {
-        cudaEventDestroy(start);
-        cudaEventDestroy(stop);
-    }
+	~GpuTimer()
+	{
+		cudaEventDestroy(start);
+		cudaEventDestroy(stop);
+	}
 
-    void Start()
-    {
-        cudaEventRecord(start, 0);
-    }
+	void Start()
+	{
+		cudaEventRecord(start, 0);                                                                 
+		cudaEventSynchronize(start);
+	}
 
-    void Stop()
-    {
-        cudaEventRecord(stop, 0);
-    }
+	void Stop()
+	{
+		cudaEventRecord(stop, 0);
+	}
 
-    float Elapsed()
-    {
-        float elapsed;
-        cudaEventSynchronize(stop);
-        cudaEventElapsedTime(&elapsed, start, stop);
-        return elapsed;
-    }
+	float Elapsed()
+	{
+		float elapsed;
+		cudaEventSynchronize(stop);
+		cudaEventElapsedTime(&elapsed, start, stop);
+		return elapsed;
+	}
 };
 
 void readPnm(char * fileName, 
-		int &width, int &height, uchar3 * &pixels)
+		int &numChannels, int &width, int &height, uint8_t * &pixels)
 {
 	FILE * f = fopen(fileName, "r");
 	if (f == NULL)
@@ -61,8 +62,11 @@ void readPnm(char * fileName,
 
 	char type[3];
 	fscanf(f, "%s", type);
-	
-	if (strcmp(type, "P3") != 0) // In this exercise, we don't touch other types
+	if (strcmp(type, "P2") == 0)
+		numChannels = 1;
+	else if (strcmp(type, "P3") == 0)
+		numChannels = 3;
+	else // In this exercise, we don't touch other types
 	{
 		fclose(f);
 		printf("Cannot read %s\n", fileName); 
@@ -71,7 +75,7 @@ void readPnm(char * fileName,
 
 	fscanf(f, "%i", &width);
 	fscanf(f, "%i", &height);
-	
+
 	int max_val;
 	fscanf(f, "%i", &max_val);
 	if (max_val > 255) // In this exercise, we assume 1 byte per value
@@ -81,14 +85,14 @@ void readPnm(char * fileName,
 		exit(EXIT_FAILURE); 
 	}
 
-	pixels = (uchar3 *)malloc(width * height * sizeof(uchar3));
-	for (int i = 0; i < width * height; i++)
-		fscanf(f, "%hhu%hhu%hhu", &pixels[i].x, &pixels[i].y, &pixels[i].z);
+	pixels = (uint8_t *)malloc(width * height * numChannels);
+	for (int i = 0; i < width * height * numChannels; i++)
+		fscanf(f, "%hhu", &pixels[i]);
 
 	fclose(f);
 }
 
-void writePnm(uchar3 * pixels, int width, int height, 
+void writePnm(uint8_t * pixels, int numChannels, int width, int height, 
 		char * fileName)
 {
 	FILE * f = fopen(fileName, "w");
@@ -98,153 +102,85 @@ void writePnm(uchar3 * pixels, int width, int height,
 		exit(EXIT_FAILURE);
 	}	
 
-	fprintf(f, "P3\n%i\n%i\n255\n", width, height); 
+	if (numChannels == 1)
+		fprintf(f, "P2\n");
+	else if (numChannels == 3)
+		fprintf(f, "P3\n");
+	else
+	{
+		fclose(f);
+		printf("Cannot write %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
 
-	for (int i = 0; i < width * height; i++)
-		fprintf(f, "%hhu\n%hhu\n%hhu\n", pixels[i].x, pixels[i].y, pixels[i].z);
-	
+	fprintf(f, "%i\n%i\n255\n", width, height); 
+
+	for (int i = 0; i < width * height * numChannels; i++)
+		fprintf(f, "%hhu\n", pixels[i]);
+
 	fclose(f);
 }
 
-__global__ void blurImgKernel(uchar3 * inPixels, int width, int height, 
-		float * filter, int filterWidth, 
-		uchar3 * outPixels)
+__global__ void convertRgb2GrayKernel(uint8_t * inPixels, int width, int height, 
+		uint8_t * outPixels)
 {
 	// TODO
+    // Reminder: gray = 0.299*red + 0.587*green + 0.114*blue  
 	int r = blockIdx.y * blockDim.y + threadIdx.y;
 	int c = blockIdx.x * blockDim.x + threadIdx.x;
 
 	if(r < height && c < width)
 	{
-		int radius = filterWidth / 2;
 		int i = r * width + c;
-		float red = 0.0;
-		float green = 0.0;
-		float blue = 0.0;
-		
-		for(int filter_row = 0; filter_row < filterWidth; filter_row++)
-		{
-			for(int filter_col = 0; filter_col < filterWidth; filter_col++)
-			{
-				int row_pixel = r - radius + filter_row;
-				int col_pixel = c - radius + filter_col;
-
-				if(row_pixel < 0)
-				{
-					row_pixel = 0;
-				}
-				else if(row_pixel > height - 1)
-				{
-					row_pixel = height - 1;
-				}
-
-				if(col_pixel < 0)
-				{
-					col_pixel = 0;
-				}
-				else if(col_pixel > width - 1)
-				{
-					col_pixel = width - 1;
-				}
-
-				int filter_idx = filter_row * filterWidth + filter_col;
-				int pixel_input = row_pixel * width + col_pixel;
-
-				red += inPixels[pixel_input].x * filter[filter_idx];
-				green += inPixels[pixel_input].y * filter[filter_idx];
-				blue += inPixels[pixel_input].z * filter[filter_idx];
-			}
-		}
-		outPixels[i].x = red;
-		outPixels[i].y = green;
-		outPixels[i].z = blue;
+		uint8_t red = inPixels[3 * i];
+		uint8_t green = inPixels[3 * i + 1];
+		uint8_t blue = inPixels[3 * i + 2];
+		outPixels[i] = 0.299*red + 0.587*green + 0.114*blue;  
 	}
+
 }
 
-void blurImg(uchar3 * inPixels, int width, int height, float * filter, int filterWidth, 
-		uchar3 * outPixels,
-		bool useDevice=false, dim3 blockSize=dim3(1, 1))
+void convertRgb2Gray(uint8_t * inPixels, int width, int height,
+		uint8_t * outPixels, 
+		bool useDevice=false, dim3 blockSize=dim3(1))
 {
 	GpuTimer timer;
 	timer.Start();
 	if (useDevice == false)
 	{
-		// TODO
-		int radius = filterWidth / 2;
-		for (int r = 0; r < height; r++)
-		{
-			for (int c = 0; c < width; c++)
-			{
-				int i = r * width + c;
-				float red = 0.0;
-				float green = 0.0;
-				float blue = 0.0;
-				
-				for(int filter_row = 0; filter_row < filterWidth; filter_row++)
-				{
-					for(int filter_col = 0; filter_col < filterWidth; filter_col++)
-					{
-						int row_pixel = r - radius + filter_row;
-						int col_pixel = c - radius + filter_col;
-						
-						if(row_pixel < 0)
-						{
-							row_pixel = 0;
-						}
-						else if(row_pixel > height - 1)
-						{
-							row_pixel = height - 1;
-						}
-
-						if(col_pixel < 0)
-						{
-							col_pixel = 0;
-						}
-						else if(col_pixel > width - 1)
-						{
-							col_pixel = width - 1;
-						}
-						
-						int filter_idx = filter_row * filterWidth + filter_col;
-						int pixel_input = row_pixel * width + col_pixel;
-
-						red += inPixels[pixel_input].x * filter[filter_idx];
-						green += inPixels[pixel_input].y * filter[filter_idx];
-						blue += inPixels[pixel_input].z * filter[filter_idx];
-					}
-				}
-
-				outPixels[i].x = red;
-				outPixels[i].y = green;
-				outPixels[i].z = blue;
-
-			}
-		}
+        // Reminder: gray = 0.299*red + 0.587*green + 0.114*blue  
+        for (int r = 0; r < height; r++)
+        {
+            for (int c = 0; c < width; c++)
+            {
+                int i = r * width + c;
+                uint8_t red = inPixels[3 * i];
+                uint8_t green = inPixels[3 * i + 1];
+                uint8_t blue = inPixels[3 * i + 2];
+                outPixels[i] = 0.299f*red + 0.587f*green + 0.114f*blue;
+            }
+        }
 	}
-	else // Use device
+	else // use device
 	{
 		cudaDeviceProp devProp;
 		cudaGetDeviceProperties(&devProp, 0);
 		printf("GPU name: %s\n", devProp.name);
 		printf("GPU compute capability: %d.%d\n", devProp.major, devProp.minor);
 
-		// TODO
-		
-		uchar3 * d_inPixels;
-		uchar3 * d_outPixels;
-		float * d_filter;
-		size_t nBytes = width * height * sizeof(uchar3);
-		size_t filter_nBytes = filterWidth * filterWidth * sizeof(float);
-
+		// TODO: Allocate device memories
+		uint8_t * d_inPixels;
+		uint8_t * d_outPixels;
+		size_t nBytes = 3 * width * height * sizeof(uint8_t); // 3 phần tử liên tiếp lần lượt là r, g, b nên x3
 		CHECK(cudaMalloc(&d_inPixels, nBytes));
 		CHECK(cudaMalloc(&d_outPixels, nBytes));
-		CHECK(cudaMalloc(&d_filter, filter_nBytes));
 
+		// TODO: Copy data to device memories
 		CHECK(cudaMemcpy(d_inPixels, inPixels, nBytes, cudaMemcpyHostToDevice));
-		CHECK(cudaMemcpy(d_filter, filter, filter_nBytes, cudaMemcpyHostToDevice));
 
+		// TODO: Set grid size and call kernel (remember to check kernel error)
 		dim3 gridSize((width - 1) / blockSize.x + 1, (height - 1) / blockSize.y + 1);
-		blurImgKernel<<<gridSize,blockSize>>>(d_inPixels, width, height, d_filter, filterWidth, d_outPixels);
+		convertRgb2GrayKernel<<<gridSize, blockSize>>>(d_inPixels, width, height, d_outPixels);
 
 		cudaError_t errSync  = cudaGetLastError();
 		cudaError_t errAsync = cudaDeviceSynchronize();
@@ -252,107 +188,77 @@ void blurImg(uchar3 * inPixels, int width, int height, float * filter, int filte
 			printf("Sync kernel error: %s\n", cudaGetErrorString(errSync));
 		if (errAsync != cudaSuccess)
 			printf("Async kernel error: %s\n", cudaGetErrorString(errAsync));
-		
+		// TODO: Copy result from device memories
 		CHECK(cudaMemcpy(outPixels, d_outPixels, nBytes, cudaMemcpyDeviceToHost));
 
+		// TODO: Free device memories
 		CHECK(cudaFree(d_inPixels));
 		CHECK(cudaFree(d_outPixels));
-		CHECK(cudaFree(d_filter));
 
 	}
 	timer.Stop();
 	float time = timer.Elapsed();
-	printf("Processing time (%s): %f ms\n", 
-    		useDevice == true? "use device" : "use host", time);
+	printf("Processing time (%s): %f ms\n\n", 
+			useDevice == true? "use device" : "use host", time);
 }
 
-float computeError(uchar3 * a1, uchar3 * a2, int n)
+float computeError(uint8_t * a1, uint8_t * a2, int n)
 {
 	float err = 0;
 	for (int i = 0; i < n; i++)
-	{
-		err += abs((int)a1[i].x - (int)a2[i].x);
-		err += abs((int)a1[i].y - (int)a2[i].y);
-		err += abs((int)a1[i].z - (int)a2[i].z);
-	}
-	err /= (n * 3);
+		err += abs((int)a1[i] - (int)a2[i]);
+	err /= n;
 	return err;
 }
 
 char * concatStr(const char * s1, const char * s2)
 {
-    char * result = (char *)malloc(strlen(s1) + strlen(s2) + 1);
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
+	char * result = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+	strcpy(result, s1);
+	strcat(result, s2);
+	return result;
 }
 
 int main(int argc, char ** argv)
-{
-	if (argc != 4 && argc != 6)
+{	
+	if (argc != 3 && argc != 5)
 	{
 		printf("The number of arguments is invalid\n");
 		return EXIT_FAILURE;
 	}
 
-	// Read input image file
-	int width, height;
-	uchar3 * inPixels;
-	readPnm(argv[1], width, height, inPixels);
+	// Read input RGB image file
+	int numChannels, width, height;
+	uint8_t * inPixels;
+	readPnm(argv[1], numChannels, width, height, inPixels);
+	if (numChannels != 3)
+		return EXIT_FAILURE; // Input image must be RGB
 	printf("Image size (width x height): %i x %i\n\n", width, height);
 
-	// Read correct output image file
-	int correctWidth, correctHeight;
-	uchar3 * correctOutPixels;
-	readPnm(argv[3], correctWidth, correctHeight, correctOutPixels);
-	if (correctWidth != width || correctHeight != height)
-	{
-		printf("The shape of the correct output image is invalid\n");
-		return EXIT_FAILURE;
-	}
+	// Convert RGB to grayscale not using device
+	uint8_t * correctOutPixels= (uint8_t *)malloc(width * height);
+	convertRgb2Gray(inPixels, width, height, correctOutPixels);
 
-	// Set up a simple filter with blurring effect 
-	int filterWidth = 9;
-	float * filter = (float *)malloc(filterWidth * filterWidth * sizeof(float));
-	for (int filterR = 0; filterR < filterWidth; filterR++)
-	{
-		for (int filterC = 0; filterC < filterWidth; filterC++)
-		{
-			filter[filterR * filterWidth + filterC] = 1. / (filterWidth * filterWidth);
-		}
-	}
-
-	// Blur input image using host
-	uchar3 * hostOutPixels = (uchar3 *)malloc(width * height * sizeof(uchar3)); 
-	blurImg(inPixels, width, height, filter, filterWidth, hostOutPixels);
-	
-	// Compute mean absolute error between host result and correct result
-	float hostErr = computeError(hostOutPixels, correctOutPixels, width * height);
-	printf("Error: %f\n\n", hostErr);
-
-	// Blur input image using device
-	uchar3 * deviceOutPixels = (uchar3 *)malloc(width * height * sizeof(uchar3));
+	// Convert RGB to grayscale using device
+	uint8_t * outPixels= (uint8_t *)malloc(width * height);
 	dim3 blockSize(32, 32); // Default
-	if (argc == 6)
+	if (argc == 5)
 	{
-		blockSize.x = atoi(argv[4]);
-		blockSize.y = atoi(argv[5]);
-	}  
-	blurImg(inPixels, width, height, filter, filterWidth, deviceOutPixels, true, blockSize);
+		blockSize.x = atoi(argv[3]);
+		blockSize.y = atoi(argv[4]);
+	} 
+	convertRgb2Gray(inPixels, width, height, outPixels, true, blockSize); 
 
-	// Compute mean absolute error between device result and correct result
-	float deviceErr = computeError(deviceOutPixels, correctOutPixels, width * height);
-	printf("Error: %f\n\n", deviceErr);
+	// Compute mean absolute error between host result and device result
+	float err = computeError(outPixels, correctOutPixels, width * height);
+	printf("Error between device result and host result: %f\n", err);
 
 	// Write results to files
 	char * outFileNameBase = strtok(argv[2], "."); // Get rid of extension
-	writePnm(hostOutPixels, width, height, concatStr(outFileNameBase, "_host.pnm"));
-	writePnm(deviceOutPixels, width, height, concatStr(outFileNameBase, "_device.pnm"));
+	writePnm(correctOutPixels, 1, width, height, concatStr(outFileNameBase, "_host.pnm"));
+	writePnm(outPixels, 1, width, height, concatStr(outFileNameBase, "_device.pnm"));
 
 	// Free memories
 	free(inPixels);
-	free(correctOutPixels);
-	free(hostOutPixels);
-	free(deviceOutPixels);
-	free(filter);
+	free(outPixels);
 }
